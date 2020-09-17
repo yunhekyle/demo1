b98902054f609cb17feac4f0b1d5e6ea8df93551
shihe <shihe@asiainfo.com>
2020-09-17 14:14:48 +0800

    demo1_基于develop的提交

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,5 +3,10 @@ import java.util.Date;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(new Date());
+
+        System.out.println(getDate());
+    }
+    public static Long getDate(){
+        return new Date().getTime();
     }
 }
